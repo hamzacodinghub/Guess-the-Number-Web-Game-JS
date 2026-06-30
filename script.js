3bb009412ed8f3784d800a8cac0dc0cd392e5fcd
Hamza Khan <hamza.codinghub@gmail.com>
2026-06-30 08:05:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 let userInp = document.querySelector("#user-inp")
 let checkBtn = document.querySelector("#check-btn");
 let resultMessage = document.querySelector("#result-message")
+let attemptCount = document.querySelector("#attempts-count")
+let count = 10;
 let secretNumber = Math.floor(Math.random() * 100) + 1;
+
 checkBtn.addEventListener('click', function () {
     let userguess = Number(userInp.value);
-    if (userguess == "") {
-        resultMessage.textContent = "Please only enter numbers"
+    if (userInp.value === "" || isNaN(userguess)) {
+        resultMessage.textContent = "Please only enter Numbers"
+        return;
     }
-    else if (userguess > secretNumber) {
-        resultMessage.textContent = "Your answer is greater"
+    else if (userguess > secretNumber && count > 0) {
+        resultMessage.textContent = "Your answer is greater";
+        count--;
+        attemptCount.textContent = `Attempts Left: ${count}`;
     }
-    else if (userguess < secretNumber) {
+    else if (userguess < secretNumber && count > 0) {
         resultMessage.textContent = "Your answer is smaller"
+        count--;
+        attemptCount.textContent = `Attempts Left: ${count}`;
     }
-    else {
+    else if (userguess === secretNumber) {
         resultMessage.textContent = "Your answer is Right"
+        return;
+    }
+    if (count === 0) {
+        resultMessage.textContent = "Game Over ! You Lost."
+        checkBtn.textContent = "Restart"
+        checkBtn.addEventListener("click", function () {
+            window.location.reload();
+            return;
+        })
     }
 })
