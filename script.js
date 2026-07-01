1d637e99318205abfa37d238bc724d4f7a926c9e
hamzacodinghub <hamza.codinghub@gmail.com>
2026-07-01 10:49:25 +0530

    added localStorage high score system with zero error math

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ let userInp = document.querySelector("#user-inp")
 let checkBtn = document.querySelector("#check-btn");
 let resultMessage = document.querySelector("#result-message")
 let attemptCount = document.querySelector("#attempts-count")
+let highScoreText = document.querySelector("#high-score-text")
 let count = 10;
 let secretNumber = Math.floor(Math.random() * 100) + 1;
+let getScore = localStorage.getItem("BestScore")
+let HighScore = getScore || 10;
+highScoreText.textContent = `Best Score: ${HighScore}`
 
 checkBtn.addEventListener('click', function () {
     let userguess = Number(userInp.value);
+    let score = 10 - count + 1;
+
     if (userInp.value === "" || isNaN(userguess)) {
         resultMessage.textContent = "Please only enter Numbers"
         return;
@@ -23,6 +29,15 @@ checkBtn.addEventListener('click', function () {
     }
     else if (userguess === secretNumber) {
         resultMessage.textContent = "Your answer is Right"
+        if (score < HighScore) {
+            HighScore = score;
+            localStorage.setItem("BestScore", HighScore);
+        }
+        checkBtn.textContent = "Restart"
+        checkBtn.addEventListener("click", function () {
+            window.location.reload();
+            return;
+        })
         return;
     }
     if (count === 0) {
